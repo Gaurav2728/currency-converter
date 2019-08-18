ac68f9bcfd5e71810611f4c05805253bd3dcaac3
Gaurav Sharma <gaurav2728@gmail.com>
2019-08-18 12:12:30 +0530

    Setup omniauth-google-oauth2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,29 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
+    if user
+      return user
+    else
+      registered_user = User.where(:email => access_token.info.email).first
+      if registered_user
+        return registered_user
+      else
+        user = User.create(name: data["name"],
+          provider:access_token.provider,
+          email: data["email"],
+          uid: access_token.uid ,
+          avatar: data["image"],
+          password: Devise.friendly_token[0,20],
+        )
+      end
+   end
+  end
+
 end
